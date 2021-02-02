678d7c3d5a9e7385096f747f03340ec8c71e2271
Javier Alcántara García <j.alcantara@dattechs.com>
2021-02-02 17:21:42 +0000

    added lab2

---
+++ lab2/index.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const path = require('path');
+
+const app = express(); 
+const public = path.join(__dirname, 'public');
+
+app.get('/', function(req, res) {
+    res.send('Hello! Welcome to my application.');
+})
+
+app.get('/guestbook', function(req, res) {
+    res.send('<h1>Guestbook Messages</h1>');
+}) 
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(public, 'about.html'));
+})
+
+app.use(express.static(public));
+app.use(function(req, res) {
+    res.status(404);
+    res.send('Oops! We didn\'t find what you are looking for.');
+})
+   
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.');
+}) 
